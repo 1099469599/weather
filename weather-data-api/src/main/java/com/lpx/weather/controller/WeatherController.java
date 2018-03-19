8640de94dc4b1ed0cab5d4d820e2e746d4467aa2
lipingxin <lipingxin@outlook.com>
2018-03-19 19:26:42 +0800

    新增天气数据api模块

---
+++ weather-data-api/src/main/java/com/lpx/weather/controller/WeatherController.java
@@ -0,0 +1,50 @@
+package com.lpx.weather.controller;
+
+import com.lpx.weather.model.Weather;
+import com.lpx.weather.service.IWeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * description:
+ * author: lpx
+ * mail: lipingxin@outlook.com
+ * time: 2018-03-14 23:19.
+ */
+
+@RestController
+@RequestMapping("/weather")
+public class WeatherController {
+
+    @Autowired
+    private IWeatherService weatherService;
+
+    // 查询未来三天的天气
+    @GetMapping("future/{city}")
+    public Weather getFutureWeather(@PathVariable String city) {
+        return weatherService.getFutureWeather(city);
+    }
+
+    // 查询即时天气
+    @GetMapping("current/{city}")
+    public Weather getCurrentWeather(@PathVariable String city) {
+        return weatherService.getCurrentWeather(city);
+    }
+
+    //查询天气（未来+即时）
+    @GetMapping("{city}")
+    public ModelAndView getWeatherReport(@PathVariable String city, Model model) {
+        model.addAttribute("title", "天气预报");
+        model.addAttribute("city", city);
+//        model.addAttribute("cityList", cityDataService.getCityList());
+        model.addAttribute("future", getFutureWeather(city));
+        model.addAttribute("current", getCurrentWeather(city));
+        return new ModelAndView("weather/weather", "weatherData", model);
+    }
+
+}
---
+++ weather-data-api/src/main/java/com/lpx/weather/service/WeatherServiceImpl.java
@@ -0,0 +1,68 @@
+package com.lpx.weather.service;
+
+import com.lpx.weather.model.Weather;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+/**
+ * description:
+ * author: lpx
+ * mail: lipingxin@outlook.com
+ * time: 2018-03-14 22:39.
+ */
+@Service
+public class WeatherServiceImpl implements IWeatherService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WeatherServiceImpl.class);
+
+    private static final String BASE_URL = "https://free-api.heweather.com/s6/";
+    private static final String FUTURE_WEATHER_REDIS_KEY = "future-weather-";
+    private static final String CURRENT_WEATHER_REDIS_KEY = "current-weather-";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Override
+    public Weather getFutureWeather(String city) {
+
+        // 从缓存中查询天气数据
+        String key = FUTURE_WEATHER_REDIS_KEY + city;
+        Weather weather = queryFromRedis(key);
+        if (weather != null) {
+            return weather;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Weather getCurrentWeather(String city) {
+
+        // 从缓存中查询天气数据
+        String key = CURRENT_WEATHER_REDIS_KEY + city;
+        Weather weather = queryFromRedis(key);
+        if (weather != null) {
+            return weather;
+        }
+
+        return weather;
+    }
+
+
+    // 从缓存中查询天气数据
+    private Weather queryFromRedis(String key) {
+        if (redisTemplate.hasKey(key)) {
+            ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+            Weather weather = (Weather) valueOperations.get(key);
+//            Weather weather = JSONObject.parseObject(weatherData, Weather.class);
+            LOGGER.info("************************ 从Redis中取到数据 ************************");
+            return weather;
+        }
+        return null;
+    }
+
+}
