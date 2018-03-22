04918c424d4d65ea24fcbf27e765cbde48c1ef41
lipingxin <lipingxin@outlook.com>
2018-03-22 11:29:44 +0800

    '将四个子模块改为eureka-client，并注册到eureka-server'

--- weather-report-server/src/main/java/com/lpx/weather/WeatherReportServerApplication.java
+++ weather-report-server/src/main/java/com/lpx/weather/WeatherReportServerApplication.java
@@ -1,9 +1,11 @@
-package com.lpx.weather.weatherreportserver;
+package com.lpx.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WeatherReportServerApplication {
 
 	public static void main(String[] args) {
